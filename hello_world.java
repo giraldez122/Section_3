72fb07be4a301783987cbda6af0a4fed2d9d1cdc
giraldez122 <giraldez122@gmail.com>
2016-10-27 11:32:05 +0200

    Update hello_world.java
    Change ":" to "{ }" in the definition of public class

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,8 @@
-public class hello_world;
+public class hello_world{
 
-public static void main (String[] args){
+  public static void main (String[] args){
 
-System.out.println("Hello World");
+    System.out.println("Hello World");
 
+  }
 }
